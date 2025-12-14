9fdeebfe7f14301c0cda332e32ccfec58aaa3484
Simone <135957682+Simo095@users.noreply.github.com>
2025-12-14 08:50:05 +0100

    fix add export default to API route getMenu.js and add a response status

--- src/pages/api/getMenu.js
+++ src/pages/api/getMenu.js
@@ -2,7 +2,15 @@ import { list } from "@vercel/blob";
 
 export const runtime = "edge";
 
-export async function GET(request) {
-  const { blobs } = await list();
-  return Response.json(blobs);
+export default async function GET(request) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    const { blobs } = await list();
+    return res.status(200).json(blobs);
+  } catch (error) {
+    res.status(500).json({ error: "Errore server" });
+  }
 }
